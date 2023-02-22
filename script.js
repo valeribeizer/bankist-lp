1b4184f8e51d0d45b51a90b8207254150de8a08c
Valeryia Beizer <strelcova.lera94@gmail.com>
2023-02-22 13:22:25 +0100

    added sticky nav bar, animated appearance of the sections

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const allSections = document.querySelectorAll('.section');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -39,7 +42,7 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-document.querySelector('.nav__links').addEventListener('click', function(e) {
+document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
@@ -47,7 +50,7 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
   }
 });
 
-tabsContainer.addEventListener('click', function(e) {
+tabsContainer.addEventListener('click', function (e) {
   e.preventDefault();
   const clicked = e.target.closest('.operations__tab');
 
@@ -57,7 +60,61 @@ tabsContainer.addEventListener('click', function(e) {
   clicked.classList.add('operations__tab--active');
 
   tabsContent.forEach(tc => tc.classList.remove('operations__content--active'));
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
 
+function handleHover(e, op) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
 
+    siblings.forEach(function (s) {
+      if (s !== link) s.style.opacity = op;
+    });
+    logo.style.opacity = op;
+  }
+}
+
+nav.addEventListener('mouseover', function (e) {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener('mouseout', function (e) {
+  handleHover(e, 1);
+});
+
+function stickyNav(entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const headerObs = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
+});
+
+headerObs.observe(header);
+
+function revealSec(entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObs = new IntersectionObserver(revealSec, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function(section) {
+  sectionObs.observe(section);
+  section.classList.add('section--hidden')
+});
